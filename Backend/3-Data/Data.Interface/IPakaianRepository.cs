caed1cf262d00e72d6d83bdf7b35aa96131fb325
Rahmat Hadiyatul Akbar <rahmat.hadiyatulakbar@nawadata.id>
2023-06-12 23:09:49 +0700

    Created New Backend

--- Backend/3-Data/Data.Interface/IPakaianRepository.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BuWanluWeb._3_Data.Data.Interface
-{
-    public interface IPakaianRepository
-    {
-        public string GetPakaian();
-        public string GetPakaianOrderByPrice(string urutan);
-    }
-}
--- Backend/3-Data/Data.Interface/IPelangganRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace BuWanluWeb._3_Data.Data.Interface
-{
-    public interface IPelangganRepository
-    {
-        public string GetPelanggan();
-        public string GetDataTerlama();
-        public string GetDataTerbaru();
-    }
-}
